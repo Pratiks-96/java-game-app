e8189a96596cccaaca929150f2d71646a8d95622
Pratiks-96 <pratiksavant96@gmail.com>
2026-02-03 23:11:24 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@ async function playGame() {
   const number = document.getElementById("numberInput").value;
   const result = document.getElementById("result");
 
-  const response = await fetch(`http://localhost:8080/api/game/guess?number=${number}`);
+  const response = await fetch(`http://3.92.66.58:8080/api/game/guess?number=${number}`);
   const text = await response.text();
 
   result.innerText = text;
